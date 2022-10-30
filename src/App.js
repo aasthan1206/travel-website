5b9ccf7af5e116f3f46725f5e62fd22a80dec3b1
aasthan1206 <aastha.nayak2002@gmail.com>
2022-10-30 13:57:45 +0530

    Initial routing with sample pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Place from './pages/Place'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+		<Router>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Home />} ></Route>
+					<Route path="/About" element={<About />} ></Route>
+					<Route path="/Place" element={<Place />} ></Route>
+				</Routes>
+      		</Layout>
+    	</Router>
+    </>
+  )
 }
 
 export default App;
